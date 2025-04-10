fb157cb11af93bde1c799d400855561856d17349
Adeyinka012 <hassee4u@gmail.com>
2025-04-10 20:08:02 +0100

    loops

---
+++ number.py
@@ -0,0 +1,16 @@
+num = int(input("Enter the number whose sum you want to find:  "))
+sum =0
+for i in range(1, num+1):
+    sum=sum+i
+    print("\nSum =",sum)
+
+string = input("Please enter your own string:  ")
+string2=('')
+for i in string:
+    string2=i +string2
+print('\nThe original string = ', string)
+print('\nthe reverse string =  ', string2)
+
+    
+        
+        
