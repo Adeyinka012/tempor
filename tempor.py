60f72d786be1544a56693406daf7a5f940aa9be1
Adeyinka012 <hassee4u@gmail.com>
2025-04-10 20:04:47 +0100

    master

---
+++ tempor.py
@@ -0,0 +1,20 @@
+# medicalcause= input("Did you have a medical cuase to miss exam(Yes/No)").inner()
+# atten= int(input("Enter the attendance of the student:  "))
+# if medicalcause == "yes" :
+#     print("You are allowed to redo the exam")
+# else:
+#     if atten >= 75:
+#         print("You are allowed to take the exam")
+#     else:
+#         print("you are not allowed to take the exam")   
+
+
+ride= input("Do you need a ride(yes/no)")
+noneed= int(input("options:90  "))
+if ride == "yes" :
+    print("what type of car do youn want to ride in")
+else:
+    if noneed >= 75:
+        print("ok")
+    else:
+        print("ok we are on are way")         
