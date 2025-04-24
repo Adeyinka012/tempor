61346aa5731b08f465e6260cf0e411f660bb8fca
Adeyinka012 <hassee4u@gmail.com>
2025-04-24 20:48:49 +0100

    yay

---
+++ intro.py
@@ -0,0 +1,44 @@
+# import turtle
+# # lol= turtle.Screen()
+# # lol.bgcolor("lightblue")
+# # lol.title("Turtl=e")
+# # my_pen=turtle.Turtle()
+# # size=0
+# # while True:
+# #     for i in range(6):
+# #         my_pen.fd(size +1)
+# #         my_pen.left(90)
+# #         size=size-5
+# #     size = size +1
+
+# turtle.Screen().bgcolor("blue")
+# board=turtle.Turtle()
+# board.forward(100)
+# board.left(120)
+# board.forward(100)
+# board.left(120)
+# board.forward(100)
+# board.penup()
+# board.right(150)
+# board.forward(50) 
+# board.pendown()
+# board.right(90)
+# board.forward(100)
+
+# board.right(120)
+# board.forward(100) 
+# board.right(120)
+# board.forward(100)
+# turtle.done() 
+
+import turtle
+turtle.Screen().bgcolor("orange")
+board=turtle.Turtle()
+board.forward(100)
+board.left(90)
+board.forward(150)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(150)
+turtle.done()
