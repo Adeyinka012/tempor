95f4ef2aa586c67eb5c887cabbf5612101e59945
Adeyinka012 <hassee4u@gmail.com>
2025-05-22 20:40:36 +0100

    tll

---
+++ tuples.py
@@ -0,0 +1,25 @@
+tuple1=("zakir", "Fareed", "david",False,3,5, 5,10)
+print(tuple1)
+print(len(tuple1))
+print(tuple1.count(5))
+tuple2=("h","g","y")
+tuple3=tuple1 +tuple2
+print(tuple3)
+print(tuple1[2])
+print(tuple1[-1])
+print(tuple1[1:4])
+
+
+weather =(1,0,1,0,0,1,1,1)
+sunny=0
+rainy=0
+for i in range(0,8):
+    if(weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
+if(sunny > rainy):
+    print("The weather is good today you do not need unbrella")
+else:
+    print("please carry your umbrella its raining too much today")
+    
